174e90c5ecab803f533368c60529816a47f0427e
raveenamanghnani <raveena.manghnani@rapidops.com>
2024-12-04 09:38:48 +0530

    light sensor updated.

--- app/src/main/java/com/example/accelerometerdemo/MainActivity.java
+++ app/src/main/java/com/example/accelerometerdemo/MainActivity.java
@@ -9,33 +9,39 @@ import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
-    private TextView x,y,z;
-    private Sensor acc;
+//    private TextView x,y,z;
+//    private Sensor acc;
+    private TextView light,lightType;
+    private Sensor lightSensor;
     private SensorManager sensorManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        x =findViewById(R.id.x_value);
-        y =findViewById(R.id.y_value);
-        z =findViewById(R.id.z_value);
+//        x =findViewById(R.id.x_value);
+//        y =findViewById(R.id.y_value);
+//        z =findViewById(R.id.z_value);
+        light =findViewById(R.id.lightLevel);
+        lightType = findViewById(R.id.lightType);
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         if(sensorManager!= null){
-            acc = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+//            acc = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        sensorManager.registerListener(this,acc,SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this,lightSensor,SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
@@ -46,13 +52,26 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
-        float x_val = sensorEvent.values[0];
-        float y_val = sensorEvent.values[1];
-        float z_val = sensorEvent.values[2];
-        x.setText("X = "+x_val);
-        y.setText("Y = "+y_val);
-        z.setText("Z = "+z_val);
-
+//        float x_val = sensorEvent.values[0];
+//        float y_val = sensorEvent.values[1];
+//        float z_val = sensorEvent.values[2];
+//        x.setText("X = "+x_val);
+//        y.setText("Y = "+y_val);
+//        z.setText("Z = "+z_val);
+        float lightLevel = sensorEvent.values[0];
+        light.setText("Light level = "+ lightLevel);
+        String lightCondition ;
+        if(lightLevel < 10){
+            lightCondition = "Dark";
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        } else if (lightLevel<1000){
+            lightCondition = "Dim";
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        } else{
+            lightCondition = "Bright";
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
+        lightType.setText("Light condition ="+ lightCondition);
     }
 
     @Override
